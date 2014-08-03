a50e9de9ed2af39db544c9de4f45fd3630e7dc83
RationalAsh <ashwinnarayan1994@gmail.com>
2014-08-03 13:37:08 +0530

    Update EXP3.m

--- Lab3/EXP3.m
+++ Lab3/EXP3.m
@@ -1,9 +1,13 @@
 Fs = 10000;
 Ts = 1/Fs;
 
-A = [100 200 1000];
-f = [40 50 10];
+A = [40 50 20];
+f = [100 200 1000];
 
-X = zeros(1, length(n));
-for i=1:500
-    
+n = 1:1000;
+x = zeros(1,length(n));
+for i=1:3
+    x = x + A(i)*sin(2*pi*f(i)*n*Ts);
+end
+
+plot(x)
